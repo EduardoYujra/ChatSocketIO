8583a6423df204168d7069490ce013de7cf7d22f
eduardo <eduardo.y.t@hotmail.com>
2020-09-18 15:18:25 -0300

    chat among us

--- index.js
+++ index.js
@@ -1,18 +1,73 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Setup basic express server
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+var numUsers = 0;
+
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  // Escucho el evento "new message" y procedo a compartir el event "new messsage" a los demas clientes
+  socket.on('new message', (data) => {
+    
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
-});
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+  // Escucho el evento "add user" y procedo a agregar la informacion 
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    socket.username = username;//guardamos la informacion en la sesion del socket del cliente
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {//emitimos el nuevo numero de usuarios activos
+      numUsers: numUsers
+    });
+    //ademas mostramos quien se unio a la sala de chat
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // emitir la accion de "typing" a los demas usuarios
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // lo mismo que antes pero cuando el se deja de escribir
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // Escuchamos el evento de desconexion 
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // Informamos que el usuario dejo la sala de chat
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
